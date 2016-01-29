b2f55e31ecaf3c7bae05c7d53665007a73594a91
Matt Beal <mrbeal15@gmail.com>
2016-01-29 17:39:07 -0600

    remove turbolinks refernce so parallax now displays without refreshing page

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-
   $('.parallax').parallax();
+
+  $(".links").on('click', function(){
+    console.log("LINK HAS BEEN CLICKED")
+    $('.parallax').parallax();
+  })
 
   $('.footer a').on('click', function(event){
     $('.parallax').parallax();
