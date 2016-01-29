b3f13ca4e1e7024690f2605f8d42cd28e7c8cf96
Matt Beal <mrbeal15@gmail.com>
2016-01-28 19:28:15 -0600

    add all links and contact form

---
+++ app/assets/javascripts/entries.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.footer a').on('click', function(event){
+    $('parallax').parallax();
+  })
+
+  $('.home a').on('click', function(event){
+    $('parallax').parallax();
+  })
+})
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,8 +1,24 @@
-
-  $(".button-collapse").sideNav({
-
-  });
-
 $(document).ready(function(){
+  $(".button-collapse").sideNav();
   $(".parallax").parallax();
+
+  $(".contact_link").on('click', function(event){
+    event.preventDefault();
+    $(".contact").show();
+    $('#textarea1').val('New Text');
+    $('#textarea1').trigger('autoresize');
+
+  })
+
+  $(".contact a").on('click', function(event){
+    event.preventDefault();
+    $(".contact").hide();
+    $(".link").show();
+  })
+
+
+  $('.footer a').on('click', function(){
+    $(".button-collapse").sideNav();
+    $(".parallax").parallax();
+  })
 });
