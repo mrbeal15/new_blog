7c404df908ce8bfe5f5cb2f723ed76b5b5053d91
Matt Beal <mrbeal15@gmail.com>
2016-01-28 21:02:34 -0600

    format blog index page, but parallax still does not load without refreshing the page -___-

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
+
+  $('.parallax').parallax();
+
   $('.footer a').on('click', function(event){
-    $('parallax').parallax();
+    $('.parallax').parallax();
   })
 
   $('.home a').on('click', function(event){
-    $('parallax').parallax();
+    $('.parallax').parallax();
   })
 })
