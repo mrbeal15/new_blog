f59951c2b9984e99cc607b224d8361da886460f1
Matt Beal <mrbeal15@gmail.com>
2016-02-22 21:44:15 -0600

    refactor auth, everything seems to be working.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.find_by(params[:username])
+    if @admin.authenticate(params[:sessions][:password])
+      session[:current_user_id] = @admin.id
+      redirect_to root_path
+    else
+      @error = "Invalid Credentials. Please try again."
+      redirect_to sessions_new_path
+    end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    flash[:notice] = "You have logged out."
+    redirect_to root_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:sessions).permit(:username, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'index#index'
   resources :entries
-  resources :login
+  resources :sessions
+
+  post '/logout' => 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
